bd899a3ce2eca1106056587a39b57a452d0373ff
Stefan Nedelcu <nedelcu.stefan17@yahoo.com>
2022-05-11 12:58:48 +0300

    Move import logic into event handler

--- src/Application/Apps/EventHandlers/InitialRevisionImport.cs
+++ src/Application/Apps/EventHandlers/InitialRevisionImport.cs
@@ -1,3 +1,4 @@
+using Hippo.Application.Common.Interfaces;
 using Hippo.Application.Common.Models;
 using Hippo.Application.Revisions.Commands;
 using Hippo.Core.Entities;
@@ -9,12 +10,19 @@ namespace Hippo.Application.Apps.EventHandlers;
 
 public class InitialRevisionImport : INotificationHandler<DomainEventNotification<CreatedEvent<App>>>
 {
+    private readonly IBindleService _bindleService;
+    private readonly IApplicationDbContext _context;
     private readonly ILogger<InitialRevisionImport> _logger;
     private readonly IMediator _mediator;
 
-    public InitialRevisionImport(ILogger<InitialRevisionImport> logger, IMediator mediator)
+    public InitialRevisionImport(IBindleService bindleService,
+        IApplicationDbContext context,
+        ILogger<InitialRevisionImport> logger,
+        IMediator mediator)
     {
-        _logger = logger;
+        _bindleService = bindleService;
+        _context = context;
+        _logger = logger;        
         _mediator = mediator;
     }
 
@@ -23,13 +31,30 @@ public class InitialRevisionImport : INotificationHandler<DomainEventNotificatio
         var domainEvent = notification.DomainEvent;
         var app = domainEvent.Entity;
 
-        var command = new ImportRevisionsCommand
-        {
-            AppId = app.Id,
-        };
+        var allAppRevisions = await _bindleService.GetBindleRevisionNumbers(app.StorageId);
+        var existingRevisions = _context.Revisions.Where(r => r.AppId == app.Id).ToList();
+        var missingRevisions = GetMissingRevisions(allAppRevisions, existingRevisions);
 
-        await _mediator.Send(command, cancellationToken);
+        foreach (var revision in missingRevisions)
+        {
+            var command = new CreateRevisionCommand
+            {
+                AppId = app.Id,
+                RevisionNumber = revision,
+            };
+
+            await _mediator.Send(command, cancellationToken);
+        }
 
         _logger.LogInformation("Hippo Domain Event: {DomainEvent}", domainEvent.GetType().Name);
+    }
+
+    private static IEnumerable<string> GetMissingRevisions(IEnumerable<string?> allAppRevisions, List<Revision> existingRevisions)
+    {
+        return allAppRevisions
+            .Where(revision => !existingRevisions.Any(er => er.RevisionNumber == revision))
+            .Where(revision => revision is not null)
+            .Select(revision => new string(revision))
+            .ToList();
     }
 }
